12f386db5f07e998469f4d83423c62cef0bc189f
Wiktor Morawski <wiktor.morawski2000@onet.pl>
2020-11-02 19:10:01 +0100

    pass 4 tests

--- lab5_zad1/tests/test_zad1.py
+++ lab5_zad1/tests/test_zad1.py
@@ -11,10 +11,10 @@ class HammingTest(unittest.TestCase):
     def test_single_letter_different_strands(self):
         self.assertEqual(hamming.distance("G", "T"), 1)
 
-    '''def test_long_identical_strands(self):
+    def test_long_identical_strands(self):
         self.assertEqual(hamming.distance("GGACTGAAATCTG", "GGACTGAAATCTG"), 0)
 
-    def test_long_different_strands(self):
+    '''def test_long_different_strands(self):
         self.assertEqual(hamming.distance("GGACGGATTCTG", "AGGACGGATTCT"), 9)
 
     def test_disallow_first_strand_longer(self):
