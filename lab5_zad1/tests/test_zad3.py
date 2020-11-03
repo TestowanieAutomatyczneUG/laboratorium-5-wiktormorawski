474ea6a617ddd9f84ee5a5866fed307b2ae165f2
Wiktor Morawski <wiktor.morawski2000@onet.pl>
2020-11-03 23:10:46 +0100

    doesnt pass 9th and 10th test

--- lab5_zad1/tests/test_zad3.py
+++ lab5_zad1/tests/test_zad3.py
@@ -31,16 +31,27 @@ class RomanNumeralsTest(unittest.TestCase):
             self.temp.byLine("Wiktor")
 
     def test_byLines_interval_from_1_to_3(self):
-        self.assertEqual(self.temp.byLineInterval(1, 3), 'On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.' +
-                       'On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.' +
-                       'On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.' )
+        self.assertEqual(self.temp.byLineInterval(1, 3),
+                         'On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.' +
+                         'On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.' +
+                         'On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.')
 
     def test_byLines_dont_accept_lower_first_value(self):
         with self.assertRaisesWithMessage(ValueError):
-            self.temp.byLineInterval(0,3)
+            self.temp.byLineInterval(0, 3)
+
     def test_byLines_dont_accept_higher_second_value(self):
         with self.assertRaisesWithMessage(ValueError):
-            self.temp.byLineInterval(12,14)
+            self.temp.byLineInterval(12, 14)
+
+    def test_byLines_dont_accept_lower_second_value(self):
+        with self.assertRaisesWithMessage(ValueError):
+            self.temp.byLineInterval(2, 1)
+
+    def test_byLines_dont_accept_higher_first_value(self):
+        with self.assertRaisesWithMessage(ValueError):
+            self.temp.byLineInterval(14, 8)
+
     def assertRaisesWithMessage(self, exception):
         return self.assertRaisesRegex(exception, r".+")
 
