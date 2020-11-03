50ac031d500278ff84d677830d79f127ab12e9ff
Wiktor Morawski <wiktor.morawski2000@onet.pl>
2020-11-03 22:54:27 +0100

    doesnt pass 7th test

--- lab5_zad1/tests/test_zad3.py
+++ lab5_zad1/tests/test_zad3.py
@@ -33,6 +33,9 @@ class RomanNumeralsTest(unittest.TestCase):
     def test_byLines_interval_from_1_to_3(self):
         self.assertEqual(self.temp.byLineInterval(1, 3), 'On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.' )
 
+    def test_byLines_dont_accept_lower_value(self):
+        with self.assertRaisesWithMessage(ValueError):
+            self.temp.byLineInterval(0,3)
     def assertRaisesWithMessage(self, exception):
         return self.assertRaisesRegex(exception, r".+")
 
